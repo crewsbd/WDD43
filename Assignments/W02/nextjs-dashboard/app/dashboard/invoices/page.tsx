5ad6a645e55387bb7c17931fbf2c7680c887f96d
Brian Crews <bdcrews@protonmail.com>
2024-09-30 14:12:37 -0700

    Ch16  Adding Metadata

--- Assignments/W02/nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ Assignments/W02/nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -5,7 +5,12 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+import { Metadata } from 'next';
 //import { fetchInvoicesPages } from '@/app/lib/data';
+
+export const metadata: Metadata = {
+  title: 'Invoice',
+};
 
 export default async function Page({
   searchParams,
