348c246ee7e9bb0563ea5c2abca7c1968cd10232
Brian Crews <bdcrews@protonmail.com>
2024-09-30 01:19:46 -0700

    Ch14 Improving Accessibility

--- Assignments/W02/nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ Assignments/W02/nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -5,22 +5,19 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
-import { fetchInvoicesPages } from '@/app/lib/data';
-
-
+//import { fetchInvoicesPages } from '@/app/lib/data';
 
 export default async function Page({
-  searchParams, }: {
-    searchParams?: {
-      query?: string;
-      page?: string
-    };
-  }) {
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
-  const totalPages =  Number(searchParams?.page) || 1;
-
-
+  const totalPages = Number(searchParams?.page) || 1;
 
   return (
     <div className="w-full">
